e0d08d9a73f04ed2cc11e7bc7a2ce2d4ebfe5b02
aakashjoshi1998 <akashjoshi61099@gmail.com>
2021-12-02 13:15:36 +0530

    changes

---
+++ project/src/App.js
@@ -0,0 +1,64 @@
+import { Steps, Button, message } from 'antd';
+import React from 'react'
+import 'antd/dist/antd.css';
+import FormOne from './components/FormOne'
+import FormTwo from './components/FormTwo';
+import FormThree from './components/FormThree'
+const { Step } = Steps;
+
+const steps = [
+  {
+    title: 'First',
+    content: <FormOne />,
+  },
+  {
+    title: 'Second',
+    content: <FormTwo />,
+  },
+  {
+    title: 'Last',
+    content: <FormThree />,
+  },
+];
+
+const App = () => {
+  const [current, setCurrent] = React.useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  return (
+    <>
+      <Steps current={current}>
+        {steps.map(item => (
+          <Step key={item.title} title={item.title} />
+        ))}
+      </Steps>
+      <div className="steps-content">{steps[current].content}</div>
+      <div className="steps-action">
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button type="primary" onClick={() => message.success('Processing complete!')}>
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+            Previous
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ project/src/components/FormOne.jsx
@@ -0,0 +1,224 @@
+import React, { useState } from 'react';
+import {
+  Form,
+  Input,
+  InputNumber,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Space,
+  Radio,
+  Checkbox,
+  Button,
+  AutoComplete,
+} from 'antd';
+const { Option } = Select;
+const plainOptions = ['Car', 'Motorbike', 'Bicycle', 'Boat'];
+const residences = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+];
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+const FormOne = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  const onChangeHandler = () => {
+
+  }
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="86">+86</Option>
+        <Option value="87">+87</Option>
+      </Select>
+    </Form.Item>
+  );
+  const suffixSelector = (
+    <Form.Item name="suffix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="USD">$</Option>
+        <Option value="CNY">¥</Option>
+      </Select>
+    </Form.Item>
+  );
+  const [autoCompleteResult, setAutoCompleteResult] = useState([]);
+
+  const onWebsiteChange = (value) => {
+    if (!value) {
+      setAutoCompleteResult([]);
+    } else {
+      setAutoCompleteResult(['.com', '.org', '.net'].map((domain) => `${value}${domain}`));
+    }
+  };
+
+  const websiteOptions = autoCompleteResult.map((website) => ({
+    label: website,
+    value: website,
+  }));
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      initialValues={{
+        residence: ['zhejiang', 'hangzhou', 'xihu'],
+        prefix: '86',
+      }}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="firstname"
+        label="Firstname"
+        tooltip="What do you want others to call you?"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your firstname!',
+            whitespace: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="lastname"
+        label="Lastname"
+        tooltip="What do you want others to call you?"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your lastname!',
+            whitespace: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="email"
+        label="E-mail"
+        rules={[
+          {
+            type: 'email',
+            message: 'The input is not valid E-mail!',
+          },
+          {
+            required: true,
+            message: 'Please input your E-mail!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item>
+        <Radio.Group onChange={onChangeHandler}>
+          <Space direction="vertical">
+            <Radio value={1}>Female</Radio>
+            <Radio value={2}>Male</Radio>
+            <Radio value={3}>Undifferentiated</Radio>
+          </Space>
+        </Radio.Group>
+      </Form.Item>
+      <Form.Item
+        name="age"
+        label="Age"
+        rules={[
+          {
+            type: 'number',
+            message: 'The input is not valid for Age!',
+          },
+          {
+            required: true,
+            message: 'Please input your Age!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item>
+        <Checkbox.Group options={plainOptions} />
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form.Item>
+    </Form >
+  );
+};
+
+export default FormOne;
